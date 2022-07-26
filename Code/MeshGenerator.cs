bebfef305f44fc30d0dc1cdfca779ab94f60881d
Ethan John <97091148+johnmanjohnston@users.noreply.github.com>
2022-07-26 18:35:39 +0400

    Update MeshGenerator.cs

--- Code/MeshGenerator.cs
+++ Code/MeshGenerator.cs
@@ -13,11 +13,10 @@ namespace TerrainGeneration.MeshGeneration {
         public Vector3[] GenerateVertices() {
             Vector3[] vertices = new Vector3[(xSize + 1) * (ySize + 1)];
 
-            // this took so long to get right i never want to deal with crap like this ever again
             for (int z = 0, i = 0; z <= ySize; z++) {
                 for (int x = 0; x <= xSize; x++) {
                     float y = noiseTexture.GetPixel(x, z).grayscale * heightMultiplier;
-                    vertices[i] = new Vector3(x, y * 2f, z);
+                    vertices[i] = new Vector3(x, y * 2f, z); // Multiply with 2 to stregnthen the effect a bit
                     i++;
                 }
             }
@@ -28,7 +27,6 @@ namespace TerrainGeneration.MeshGeneration {
         public int[] GenerateTriangles() {
             int[] triangles = new int[xSize * ySize * 6];
 
-            // thank you github copilot you saved me from losing my sanity here
             for (int z = 0, i = 0; z < ySize; z++) {
                 for (int x = 0; x < xSize; x++) {
                     triangles[i]     = (int)((z * (xSize + 1)) + x);
